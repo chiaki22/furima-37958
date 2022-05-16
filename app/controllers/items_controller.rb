c7a3a911ff77a36b52fd6a4a9729bababfc3ed4c
chiaki22 <eriguchiaki227@gmail.com>
2022-05-16 18:08:35 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,27 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :item_name,
+      :item_info,
+      :item_category_id,
+      :item_status_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :scheduled_delivery_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 
 end
